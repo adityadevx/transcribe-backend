1b5a4cc9d1fa8e3581ffaa5b8be865147f2bab3b
Aditya Raj Chauhan <102533268+caringcoder@users.noreply.github.com>
2023-07-05 20:34:24 +0530

    push

--- routes/login.js
+++ routes/login.js
@@ -7,28 +7,19 @@ const path = require('path');
 // console.log(token)
 // const idpass = require(__dirname + '/idpass.json');
 router.post('/', async (req, res) => {
-  
-     const idpass1 = await fs.promises.readFile(__dirname +'/idpass.json');
-const idpass= JSON.parse(idpass1)    
-//  const idpass = require(__dirname + '/idpass.json');
-    const { email, password } = req.body;
-    // console.log(process.env.EMAIL, process.env.PASSWORD);
 
-    // console.log(username, password)
+    const idpass1 = await fs.promises.readFile(__dirname + '/idpass.json');
+    const idpass = JSON.parse(idpass1)
+    //  const idpass = require(__dirname + '/idpass.json');
+    const { email, password } = req.body;
+    console.log(process.env.EMAIL, process.env.PASSWORD, 'process.env.EMAIL, process.env.PASSWORD');
+
+    // console.log(email, password ,'req.body')
     if (email === idpass.email && password === idpass.password) {
         const token = crypto.randomBytes(20).toString('hex');
         fs.writeFileSync(path.join(__dirname, '/sessionid.json'), JSON.stringify({ token: token }));
         res.status(200).json({ message: 'Login successful', token: token });
     }
-
-
-
-
-    // if (email === process.env.EMAIL && password === process.env.PASSWORD) {
-    //     const token = crypto.randomBytes(20).toString('hex');
-    //     fs.writeFileSync(`./sessionid.json`, JSON.stringify({ token: token }));
-    //     res.status(200).json({ message: 'Login successful', token: token });
-    // }
     else return res.status(401).json({ message: 'Unauthorized User' });
 }
 );
